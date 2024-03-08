3daba2b9360d266417ca131f5e36c063724e0111
yassmin21 <66423533+yassmin21@users.noreply.github.com>
2024-03-08 11:05:22 -0600

    updates

--- scene/sketch.js
+++ scene/sketch.js
@@ -32,30 +32,31 @@ function draw() {
   changeState();
   if (state === "before") {
     startScreenDisplay();
-  } else if (state === "play") {
+  }
+  else if (state === "play") {
     level();
   }
   else if(state === "easy"){
     drawMaze();
     drawPlayer();
     easy();
-    }
+  }
   else if(state === "medium"){
-      drawMaze();
-      drawPlayer();
-      medium();
-    }
+    drawMaze();
+    drawPlayer();
+    medium();
+  }
   else if(state === "hard" ){
-      drawMaze();
-      drawPlayer();
-      hard();
-    }
-   else if (state === "done") {
+    drawMaze();
+    drawPlayer();
+    hard();
+  }
+  else if (state === "done") {
     done();
-  } else if(state === "loose"){
+  }
+  else if(state === "loose"){
     loose();
   }
-
 }
 
 function drawMaze() {
@@ -64,7 +65,8 @@ function drawMaze() {
     for (let j = 0; j < maze[i].length; j++) {
       if (maze[i][j] === 1) {
         fill("green");
-      } else {
+      }
+      else {
         fill(255); // Open space
       }
       rect(j * 40, i * 40, 40);
@@ -80,11 +82,14 @@ function drawPlayer() {
 function keyPressed() {
   if (keyCode === UP_ARROW && maze[player.y - 1][player.x] === 0) {
     player.y -= 1;
-  } else if (keyCode === DOWN_ARROW && maze[player.y + 1][player.x] === 0) {
+  }
+  else if (keyCode === DOWN_ARROW && maze[player.y + 1][player.x] === 0) {
     player.y += 1;
-  } else if (keyCode === LEFT_ARROW && maze[player.y][player.x - 1] === 0) {
+  }
+  else if (keyCode === LEFT_ARROW && maze[player.y][player.x - 1] === 0) {
     player.x -= 1;
-  } else if (keyCode === RIGHT_ARROW && maze[player.y][player.x + 1] === 0) {
+  }
+  else if (keyCode === RIGHT_ARROW && maze[player.y][player.x + 1] === 0) {
     player.x += 1;
   }
 }
@@ -117,10 +122,10 @@ function easy(){
 
 function medium(){
   textSize(40);
-    let millisecond = int(millis()/1000);
-    text(millisecond, 590, 34);
+  let millisecond = int(millis()/1000);
+  text(millisecond, 590, 34);
   if(state === "medium"){
-    if(milisecond > 20){
+    if(millisecond > 20){
       state = "loose";
     }
   }
@@ -130,11 +135,11 @@ function hard(){
   textSize(40);
   let millisecond = int(millis()/1000);
   text(millisecond, 590, 34);
-if(state === "hard"){
-  if(milisecond > 10){
-    state = "loose";
+  if(state === "hard"){
+    if(millisecond > 10){
+      state = "loose";
+    }
   }
-}
 }
 
 function changeState() {
@@ -152,6 +157,12 @@ function changeState() {
   }
   if(mouseIsPressed && mouseX > width/2 - 300 && mouseY > height/2 + 50 ){
     state = "easy";
+  }
+  if(mouseIsPressed && mouseX > width/ 2 - 90 && mouseY > height/2 + 50){
+    state = "medium";
+  }
+  if(mouseIsPressed){
+    
   }
 }
 
