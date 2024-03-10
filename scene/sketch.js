2f229c6d7c2d6560bbcb8dbd90fd08c829c7a7c9
yassmin21 <66423533+yassmin21@users.noreply.github.com>
2024-03-10 17:13:06 -0600

    easy medium

--- scene/sketch.js
+++ scene/sketch.js
@@ -2,8 +2,13 @@
 //draw the moving thing done
 //move it using arrows done
 // dont let it go grough walls done
-//mouse change scene idk what tho ahhhh
-//end screen
+//mouse change scene idk what tho ahhhh done
+//end screen done
+// fix bottons easy medium hard done
+// fix millis
+// make text pretty
+//syntax fix
+//make things clear
 //(640, 400)
 const maze = [
   [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -22,9 +27,11 @@ const player = {
   y: 0,
 };
 let state = "before";
+let resettime = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  S = 0;
 }
 
 function draw() {
@@ -32,9 +39,6 @@ function draw() {
   changeState();
   if (state === "before") {
     startScreenDisplay();
-  }
-  else if (state === "play") {
-    level();
   }
   else if(state === "easy"){
     drawMaze();
@@ -51,8 +55,8 @@ function draw() {
     drawPlayer();
     hard();
   }
-  else if (state === "done") {
-    done();
+  else if (state === "win") {
+    win();
   }
   else if(state === "loose"){
     loose();
@@ -61,7 +65,7 @@ function draw() {
 
 function drawMaze() {
   background("white");
-  for (let i = 0; i < maze.length; i++) {
+  for (let i =0; i < maze.length; i++) {
     for (let j = 0; j < maze[i].length; j++) {
       if (maze[i][j] === 1) {
         fill("green");
@@ -97,78 +101,81 @@ function keyPressed() {
 function startScreenDisplay() {
   background("pink");
   textSize(20);
+  rect(width/2 - 300 , height/2  - 25,  150, 40);
+  text("EASY", width / 2 - 257, height / 2);
+
   rect(width / 2 - 90, height / 2 - 25, 150, 40);
-  text("PLAY", windowWidth / 2 - 40, windowHeight / 2);
-}
+  text("MEDIUM", windowWidth / 2 - 52, windowHeight / 2);
 
-function level(){
-  rect(width/2 - 300 , height/2 + 50 ,  150, 40);
-  text("EASY", width / 2 - 257, height / 2 + 75);
-
-  rect(width / 2 - 90, height / 2 + 50, 150, 40);
-  text("MEDIUM", windowWidth / 2 - 52, windowHeight / 2 + 75);
-
-  rect(width/2 + 130 , height/2 + 50 ,  150, 40);
-  text("HARD", windowWidth / 2 + 175, windowHeight / 2 + 75);
+  rect(width/2 + 130 , height/2 -25,  150, 40);
+  text("HARD", windowWidth / 2 + 175, windowHeight / 2);
 }
 
 function easy(){
   textSize(40);
   let millisecond = int(millis()/1000);
   text(millisecond, 590, 34);
-  if(millisecond > 30){
+  if(millisecond >= 25){
     state = "loose";
   }
 }
 
 function medium(){
   textSize(40);
-  let millisecond = int(millis()/1000);
-  text(millisecond, 590, 34);
-  if(state === "medium"){
-    if(millisecond > 20){
+  // let millisecond = int(millis()/1000);
+  // let gametime = 0;
+  // if (millisecond > 0 && gametime === 0){
+  //   millisecond = 0;
+  //   gametime = millisecond
+  // }
+  let S;
+  let t=0;
+  let dt=0.1;
+  if (S===1){
+    t=t+dt;
+    }
+  text(t, 590, 34);
+  if(t >= 15){
       state = "loose";
     }
+}
+function touchStarted(){
+  if (S===0){
+  S=1;
+  }
+  else{
+  S=0;
   }
 }
 
 function hard(){
   textSize(40);
-  let gameTime = 0;
   let millisecond = int(millis()/1000);
   text(millisecond, 590, 34);
-  if(state === "hard"){
-    if(millisecond > 10){
+  if(millisecond >= 10){
       state = "loose";
+    }
+}
+
+function changeState() {
+  if (player.y * 40 === 320 && player.x * 40 === 600) {
+    state = "win";
+  }
+  
+  if(mouseIsPressed &&  mouseY > windowHeight / 2 - 25 && mouseY < windowHeight / 2 + 15){
+    if (mouseX > windowWidth / 2 - 90 && mouseX < windowWidth / 2 + 60) {
+      state = "medium";
+    }
+    if(mouseX > width/2 - 300 && mouseX < width/2 - 150){
+        state = "easy";
+      }
+    if(mouseX > width/2 + 130 && mouseX < width/2 + 280){
+        state = "hard";
     }
   }
 }
 
-function changeState() {
-  if (
-    mouseIsPressed &&
-    mouseX > windowWidth / 2 - 90 &&
-    mouseX < windowWidth / 2 + 60 &&
-    mouseY > windowHeight / 2 - 25 &&
-    mouseY < windowHeight / 2 + 15
-  ) {
-    state = "play";
-  }
-  if (player.y * 40 === 320 && player.x * 40 === 600) {
-    state = "done";
-  }
-  if(mouseIsPressed && mouseX > width/2 - 300 && mouseY > height/2 + 50 ){
-    state = "easy";
-  }
-  if(mouseIsPressed && mouseX > width/ 2 - 90 && mouseY > height/2 + 50){
-    state = "medium";
-  }
-  if(mouseIsPressed && mouseX > width/2 + 130 && mouseY > height/2 + 50){
-    state = "hard";
-  }
-}
-
-function done() {
+function win() {
   background("white");
   fill("green");
   textSize(50);
