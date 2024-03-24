d34b814909bffe4c42cde25dfc7aa59ca11c9ee7
yassmin21 <66423533+yassmin21@users.noreply.github.com>
2024-03-23 18:57:35 -0600

    added things

--- array/sketch.js
+++ array/sketch.js
@@ -4,20 +4,26 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let bird = {
+// let bird = {
  
-};
-let pillar = [];
+// };
+// let pillar;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 600);
+  // let top = random(height / 2);
+  // let bottom = random(height / 2);
+  // let w = 20;
 }
 
 function draw() {
-  background(220);
+  background(0);
+  pillar();
 }
 
-function drawpillars(){
-  rect(100, 50, 20, 20);
-
+function pillar() {
+  let pillarHeight = random(30, height/2)
+  fill("white")
+  rect(20, 0, 20, pillarHeight);
+  
 }
