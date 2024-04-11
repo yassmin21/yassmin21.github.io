6180bbce8e348542fdd7f0fcd117d36b987a4884
yassmin21 <66423533+yassmin21@users.noreply.github.com>
2024-04-11 13:51:14 -0600

    demo

--- 09-grid/sketch.js
+++ 09-grid/sketch.js
@@ -1,68 +1,105 @@
 // 2D Grid
 // April 9 24
 
-//if you are hard codimg a level:
+// 2D Grid
+// Dan Schellenberg
+// Apr 9, 2024
 
-// let grid =[
-//   [1, 0, 0, 1],
-//   [0, 1, 0, 1],
-//   [0, 0, 0, 1],
-//   [1, 1, 0, 0],
-//   [1, 0, 1, 1],
-//   [0, 0, 0, 1],
-//   [0, 0, 1, 1],
-//   [0, 1, 0, 1]
-// ];
-   
-//if randomizing the grid:
+// if you are hard-coding a level, I'd use something like this
+
+// let grid = [[1, 0, 0, 1],
+//             [0, 1, 0, 1],
+//             [1, 1, 0, 0],
+//             [1, 0, 1, 1],
+//             [0, 0, 0, 1],
+//             [0, 0, 1, 1],
+//             [0, 1, 0, 1],
+//             [0, 0, 0, 1]];
+
 let grid;
 let cellSize;
 const GRID_SIZE = 10;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  //this is dumb should check if this is the right size
+  //make the canvas the largest square
+  if(windowWidth< windowHeight){
+    createCanvas(windowWidth, windowWidth);
+  }
+  else{
+    createCanvas(windowWidth, windowHeight);
+  }
+
+  //if randomizing the grid, do this:
+  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
   
-  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE); 
+  //this is dumb -- should check if this is the right size!
   cellSize = height/grid.length;
 }
 
+function windowResized(){
+  
+  if(windowWidth< windowHeight){
+    resizeCanvas(windowWidth, windowWidth);
+  }
+  else{
+    resizeCanvas(windowWidth, windowHeight);
+  }
+  cellSize = height/grid.length;
+}
 function draw() {
   background(220);
   displayGrid();
 }
 
-function keyPressed(){
-  if(key === "r"){
-    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE); 
+function keyPressed() {
+  if (key === "r") {
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
+
+  if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+
+
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  // console.log(x, y);
+
+  //fix this so we check if we fell off the top or left sides...
+  if(key === "N"){
+    toggleCell(x, y);
+    toggleCell(x + 1, y);
+    toggleCell(x - 1, y);
+    toggleCell(x, y + 1);
+    toggleCell(x, y - 1);
+  }
+    
+}
+
+function toggleCell(x, y) {
+  if (x < GRID_SIZE && y < GRID_SIZE && x>= 0 && y>= 0) {
+  //toggle the color of the cell
+    if (grid[y][x] === 0) {
+     grid[y][x] = 1;
+     }
+    else {
+     grid[y][x] = 0;
+    }
   }
   
 }
 
-function generateRandomGrid(cols, rows){
-  let emptyArray = [];
-  for(let y = 0; y<rows; y++){
-    emptyArray.push([]);
-    for(let x = 0; x< cols; x++){
-      //half the time 1 half the time 0
-      if(random(100) < 50){
-        emptyArray[y].push(0);
-      }
-      else{
-        emptyArray[y].push(1);
-      }
-    }
-  }
-  return emptyArray;
-}
-
-function displayGrid(){
-  for(let y = 0; y< grid.length; y++){
-    for(let x = 0; x<grid[y].length; x++){
-      if(grid[y][x] === 1){
+function displayGrid() {
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x] === 1) {
         fill("black");
       }
-      else{
+      else {
         fill("white");
       }
       square(x * cellSize, y * cellSize, cellSize);
@@ -70,6 +107,30 @@ function displayGrid(){
   }
 }
 
-function mouseClicked(){
-  
+function generateRandomGrid(cols, rows) {
+  let emptyArray = [];
+  for (let y = 0; y < rows; y++) {
+    emptyArray.push([]);
+    for (let x = 0; x < cols; x++) {
+      //half the time, be a 1. Other half, be a 0.
+      if (random(100) < 50) {
+        emptyArray[y].push(0);
+      }
+      else {
+        emptyArray[y].push(1);
+      }
+    }
+  }
+  return emptyArray;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let emptyArray = [];
+  for (let y = 0; y < rows; y++) {
+    emptyArray.push([]);
+    for (let x = 0; x < cols; x++) {
+      emptyArray[y].push(0);
+    }
+  }
+  return emptyArray;
 }
