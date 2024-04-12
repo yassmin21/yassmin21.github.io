5e492d21627902b2d5d2bf2f421a8e0698c40b65
yassmin21 <66423533+yassmin21@users.noreply.github.com>
2024-04-12 14:11:26 -0600

    game of life

--- 09-grid/sketch.js
+++ 09-grid/sketch.js
@@ -1,7 +1,4 @@
 // 2D Grid
-// April 9 24
-
-// 2D Grid
 // Dan Schellenberg
 // Apr 9, 2024
 
@@ -19,14 +16,15 @@
 let grid;
 let cellSize;
 const GRID_SIZE = 10;
+let toggleStyle = "self";
 
 function setup() {
-  //make the canvas the largest square
-  if(windowWidth< windowHeight){
+  //make the canvas the largest square that you can...
+  if (windowWidth < windowHeight) {
     createCanvas(windowWidth, windowWidth);
   }
-  else{
-    createCanvas(windowWidth, windowHeight);
+  else {
+    createCanvas(windowHeight, windowHeight);
   }
 
   //if randomizing the grid, do this:
@@ -36,16 +34,18 @@ function setup() {
   cellSize = height/grid.length;
 }
 
-function windowResized(){
-  
-  if(windowWidth< windowHeight){
+function windowResized() {
+  //make the canvas the largest square that you can...
+  if (windowWidth < windowHeight) {
     resizeCanvas(windowWidth, windowWidth);
   }
-  else{
-    resizeCanvas(windowWidth, windowHeight);
+  else {
+    resizeCanvas(windowHeight, windowHeight);
   }
+
   cellSize = height/grid.length;
 }
+
 function draw() {
   background(220);
   displayGrid();
@@ -60,37 +60,43 @@ function keyPressed() {
     grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
   }
 
+  if (key === "n") {
+    toggleStyle = "neighbours";
+  }
 
+  if (key === "s") {
+    toggleStyle = "self";
+  }
 }
 
 function mousePressed() {
   let x = Math.floor(mouseX/cellSize);
   let y = Math.floor(mouseY/cellSize);
 
-  // console.log(x, y);
+  //toggle self
+  toggleCell(x, y);
 
-  //fix this so we check if we fell off the top or left sides...
-  if(key === "N"){
-    toggleCell(x, y);
+  // and NESW neighbours, if style is set to neighbours
+  if (toggleStyle === "neighbours") {
     toggleCell(x + 1, y);
     toggleCell(x - 1, y);
     toggleCell(x, y + 1);
     toggleCell(x, y - 1);
   }
-    
 }
 
 function toggleCell(x, y) {
-  if (x < GRID_SIZE && y < GRID_SIZE && x>= 0 && y>= 0) {
-  //toggle the color of the cell
+  // make sure the cell you're toggling is in the grid...
+  if (x < GRID_SIZE && y < GRID_SIZE &&
+      x >= 0 && y >= 0) {
+    //toggle the color of the cell
     if (grid[y][x] === 0) {
-     grid[y][x] = 1;
-     }
+      grid[y][x] = 1;
+    }
     else {
-     grid[y][x] = 0;
+      grid[y][x] = 0;
     }
   }
-  
 }
 
 function displayGrid() {
