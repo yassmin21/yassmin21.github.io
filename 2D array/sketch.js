e1e87a8c7a69d6aee4beeb0b29ec41f6110de2a3
yassmin21 <66423533+yassmin21@users.noreply.github.com>
2024-04-18 14:11:14 -0600

    dd

--- 2D array/sketch.js
+++ 2D array/sketch.js
@@ -15,6 +15,8 @@ let grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0]];
 let cellSize;
 
+//masking
+
 function setup() {
   //make the canvas the largest square that you can...
   if (windowWidth < windowHeight) {
@@ -26,6 +28,8 @@ function setup() {
   
   //this is dumb -- should check if this is the right size!
   cellSize = height/grid.length;
+  textSize(50);
+  textAlign(CENTER, CENTER);
 }
 
 function windowResized() {
@@ -55,13 +59,45 @@ function displayGrid() {
       //     strokeWeight(1);
       // }
       square(x * cellSize, y * cellSize, cellSize);
-      
       if(grid[y][x] === 1){
-        text("1", x * cellSize, y * cellSize);
+        
+        text("1", x * cellSize + 45, y * cellSize + 50);
+      }
+      if(grid[y][x] === 2){
+        text("2", x * cellSize + 45, y * cellSize + 50);
+      }
+      if(grid[y][x] === 3){
+        text("3", x * cellSize + 45, y * cellSize + 50);
+      }
+      if(grid[y][x] === 4){
+        text("4", x * cellSize + 45, y * cellSize + 50);
+      }
+      if(grid[y][x] === 5){
+        text("5", x * cellSize + 45, y * cellSize + 50);
+      }
+      if(grid[y][x] === 6){
+        text("6", x * cellSize + 45, y * cellSize + 50);
+      }
+      if(grid[y][x] === 7){
+        text("7", x * cellSize + 45, y * cellSize + 50);
+      }
+      if(grid[y][x] === 8){
+        text("8", x * cellSize + 45, y * cellSize + 50);
+      }
+      if(grid[y][x] === 9){
+        text("9", x * cellSize + 45, y * cellSize + 50);
       }
     }
   }
 }
+
+// function displayNumbers(){
+//   if(grid[y][x] === 1){
+//     textSize(50);
+//     textAlign(CENTER, CENTER);
+//     text("1", x * cellSize + 45, y * cellSize + 50);
+//   }
+// }
 
 function keyPressed(){
   let x = Math.floor(mouseX/cellSize);
@@ -71,5 +107,26 @@ function keyPressed(){
   }
   else if(key === "2"){
     grid[y][x] = 2;
+  }
+  else if(key === "3"){
+    grid[y][x] = 3;
+  }
+  else if(key === "4"){
+    grid[y][x] = 4;
+  }
+  else if(key === "5"){
+    grid[y][x] = 5;
+  }
+  else if(key === "6"){
+    grid[y][x] = 6;
+  }
+  else if(key === "7"){
+    grid[y][x] = 7;
+  }
+  else if(key === "8"){
+    grid[y][x] = 8;
+  }
+  else if(key === "9"){
+    grid[y][x] = 9;
   }
 }
