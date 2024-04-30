4c789e2e0dbf468a9e481d11611894b163a5d7b8
yassmin21 <66423533+yassmin21@users.noreply.github.com>
2024-04-29 22:23:53 -0600

    procrastinating stinks

--- 2D array/sketch.js
+++ 2D array/sketch.js
@@ -2,6 +2,13 @@
 // 4/15/ 2024
 // sudoku
 
+
+
+
+// make mistakes counter
+//add sound, timer and clicking
+
+//the solved grid
 let solvedGrid = [[8, 1, 6, 2, 3, 4, 5, 7, 9],
   [2, 5, 9, 7, 6, 8, 4, 3, 1],
   [4, 7, 3, 5, 9, 1, 2, 8, 6],
@@ -12,6 +19,7 @@ let solvedGrid = [[8, 1, 6, 2, 3, 4, 5, 7, 9],
   [6, 9, 5, 8, 4, 3, 7, 1, 2],
   [7, 8, 4, 1, 2, 9, 3, 6, 5]];
 
+//the editable grid
 let grid = [[8, 1, 0, 0, 3, 0, 0, 0, 0],
   [2, 5, 9, 0, 6, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 1, 2, 0, 0],
@@ -22,6 +30,7 @@ let grid = [[8, 1, 0, 0, 3, 0, 0, 0, 0],
   [0, 0, 5, 8, 0, 0, 0, 0, 0],
   [0, 0, 4, 0, 2, 0, 3, 0, 0]];
 
+//the initial grid
 let gridInitial = [[1, 1, 0, 0, 1, 0, 0, 0, 0],
   [1, 1, 1, 0, 1, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 1, 1, 0, 0],
@@ -36,8 +45,12 @@ let cellSize;
 const OPEN_TILE = 0;
 const IMPASSIBLE = 1;
 let state = "startScreen";
+let xBotton;
 
-//masking
+function preload(){
+  howToBackground = loadImage("how to background.png");
+  backgroundStartScreen = loadImage("background.png");
+}
 
 function setup() {
   //make the canvas the largest square that you can...
@@ -50,14 +63,13 @@ function setup() {
   
   cellSize = height/grid.length;
 
-
+  
   textSize(cellSize*0.5);
   textAlign(CENTER, CENTER);
-  
+  xBotton = height/ 4;
 }
-
 function windowResized() {
-  //make the canvas the largest square that you can...
+  //make the canvas the largest square that you can
   if (windowWidth < windowHeight) {
     resizeCanvas(windowWidth, windowWidth);
   }
@@ -65,11 +77,18 @@ function windowResized() {
     resizeCanvas(windowHeight, windowHeight);
   }
 
+  
   cellSize = height/grid.length;
+
+  //makes the text be smaller as screen smaller
   textSize(cellSize*0.5);
+  
+  //resests the height between the text in startscreen
+  xBotton = height/4;
 }
 
 function draw() {
+  changeState();
   if(state === "startScreen"){
     startScreen();
   }
@@ -87,36 +106,48 @@ function draw() {
     displayNumbers();
   }
   
-  
 }
 
 function startScreen(){
-  background("pink");
-  rect(width/2 - 70, height/2 - 25, 150, 50);
-  text("PLAY", width/2, height/2);
-  rect(width/2 - 70, height/2 + 50, 150, 50);
-  text("How To Play", width/2, height/2 + 75);
-  if(mouseIsPressed && state === "startScreen" 
-     && mouseX > width/2 - 70 
-     && mouseY > height/2 - 25
-     && mouseX < width/2 + 80 
-     && mouseY < height/2 + 25){
-    state = "playing";
-  }
-  if(mouseIsPressed && state === "startScreen" && mouseX > width/2 - 70 && mouseY> height/2 + 50 && mouseX < width/2 + 80 && mouseY < height/2 + 125){
-    state = "howToPlay";
-  }
+  background(backgroundStartScreen);
+  textFont("DIDOT");
+  fill("black");
+  textStyle(BOLD);
+  text("SUDOKU", width/2, height/2 - xBotton);
+  textStyle(NORMAL);
+  text("Play", width/2, height/2);
+  text("How To Play", width/2, height/2 + xBotton); 
 }
+
 function HowToPlay(){
-  background("grey");
+  background(howToBackground);
 }
   
-
+//sets the grid to the solved grid
 function solve(){
   grid = solvedGrid;
 }
 
+//changes state when the mouse is pressed in start screen
+function changeState(){
+  if(mouseIsPressed && state === "startScreen"){
+    if(mouseX > width/2 - 40 
+    && mouseY > height/2 - 20
+    && mouseX < width/2 + 40 
+    && mouseY < height/2 + 20){
+      state = "playing";
+    }
+    else if(mouseX > width/2 - 80
+    && mouseY > height/2 + xBotton - 20
+    && mouseX < width/2 + 80 
+    && mouseY < height/2 + xBotton + 20){
+    state = "howToPlay";
+    }
+  }
+     
+}
 
+//draws lines
 function lines(){
   strokeWeight(2);
   line(cellSize*3, 0, cellSize*3, height);
@@ -125,15 +156,18 @@ function lines(){
   line(0, cellSize* 6, width, cellSize * 6);
 }
 
+
 function displayGrid() {
   strokeWeight(1);
-  
   for (let y = 0; y < gridInitial.length; y++) {
     for (let x = 0; x < gridInitial[y].length; x++) {
+
+      //makes all the squares white
       fill("white");
       square(x * cellSize, y * cellSize, cellSize);
-      if (state === "typing" || state === "playing") {
-        
+
+      //if the state is playing or typing it adds a light blue highlight to the square you are on with the mouse
+      if (state === "playing" || state === "typing") {
         let x = Math.floor(mouseX/cellSize);
         let y = Math.floor(mouseY/cellSize);
         fill(173, 216, 230);
@@ -143,6 +177,7 @@ function displayGrid() {
   }
 }
 
+//the state changes from playing to typing when the mouse is clicked
 function mouseClicked(){
   if(state === "playing"){
     state = "typing";
@@ -153,135 +188,68 @@ function mouseClicked(){
 }
 
 function displayNumbers(){
-  cellSize = height/grid.length;
+  //goes through each element in the grid array
   for(let y = 0; y< grid.length; y++){
     for(let x = 0; x< grid[y].length; x++){
+      // constent variables for where x and y for the text are
       let xText = x * cellSize + cellSize/2;
       let yText = y * cellSize + cellSize/2;
+
+      fill("black");
+
+      //checks to see if this is in the initial grid or not and if it is then it makes it bold
+      if(gridInitial[y][x] === IMPASSIBLE){
+        textStyle(BOLD);
+      }
+      else if(gridInitial[y][x] === OPEN_TILE){
+        textStyle(NORMAL);
+      }
+
+      //displays the number on the grid
       if(grid[y][x] === 1){
-        
-        if(gridInitial[y][x] === 1){
-          textStyle(BOLD);
-          fill("black");
-          text("1", xText , yText);
-        }
-        else if(gridInitial[y][x] === 0){
-          textStyle(NORMAL);
-          fill("black");
-          text("1", xText , yText);
-        }
+        text("1", xText , yText);
       }
-      if(grid[y][x] === 2){
-        if(gridInitial[y][x] === 1){
-          textStyle(BOLD);
-          fill("black");
-          text("2", xText , yText);
-        }
-        else if(gridInitial[y][x] === 0){
-          textStyle(NORMAL);
-          fill("black");
-          text("2", xText , yText);
-        }
+      else if(grid[y][x] === 2){
+       text("2", xText , yText);
       }
-      if(grid[y][x] === 3){
-        if(gridInitial[y][x] === 1){
-          textStyle(BOLD);
-          fill("black");
-          text("3", xText , yText);
-        }
-        else if(gridInitial[y][x] === 0){
-          textStyle(NORMAL);
-          fill("black");
-          text("3", xText , yText);
-        }
+      else if(grid[y][x] === 3){
+        text("3", xText , yText);
       }
-      if(grid[y][x] === 4){
-        if(gridInitial[y][x] === 1){
-          textStyle(BOLD);
-          fill("black");
-          text("4", xText , yText);
-        }
-        else if(gridInitial[y][x] === 0){
-          textStyle(NORMAL);
-          fill("black");
-          text("4", xText , yText);
-        }
+      else if(grid[y][x] === 4){
+        text("4", xText , yText);
       }
-      if(grid[y][x] === 5){
-        if(gridInitial[y][x] === 1){
-          textStyle(BOLD);
-          fill("black");
-          text("5", xText , yText);
-        }
-        else if(gridInitial[y][x] === 0){
-          textStyle(NORMAL);
-          fill("black");
-          text("5", xText , yText);
-        }
+      else if(grid[y][x] === 5){
+        text("5", xText , yText);
       }
-      if(grid[y][x] === 6){
-        if(gridInitial[y][x] === 1){
-          textStyle(BOLD);
-          fill("black");
-          text("6", xText , yText);
-        }
-        else if(gridInitial[y][x] === 0){
-          textStyle(NORMAL);
-          fill("black");
-          text("6", xText , yText);
-        }
+      else if(grid[y][x] === 6){
+        text("6", xText , yText);
       }
-      if(grid[y][x] === 7){
-        if(gridInitial[y][x] === 1){
-          textStyle(BOLD);
-          fill("black");
-          text("7", xText , yText);
-        }
-        else if(gridInitial[y][x] === 0){
-          textStyle(NORMAL);
-          fill("black");
-          text("7", xText , yText);
-        }
+      else if(grid[y][x] === 7){
+        text("7", xText , yText);
       }
-      if(grid[y][x] === 8){
-        if(gridInitial[y][x] === 1){
-          textStyle(BOLD);
-          fill("black");
-          text("8", xText , yText);
-        }
-        else if(gridInitial[y][x] === 0){
-          textStyle(NORMAL);
-          fill("black");
-          text("8", xText , yText);
-        }
-      }
-      if(grid[y][x] === 9){
-        if(gridInitial[y][x] === 1){
-          textStyle(BOLD);
-          fill("black");
-          text("9", xText , yText);
-        }
-        else if(gridInitial[y][x] === 0){
-          textStyle(NORMAL);
-          fill("black");
-          text("9", xText , yText);
-        }
+      else if(grid[y][x] === 8){
+        text("8", xText , yText);
+     }
+      else if(grid[y][x] === 9){
+        text("9", xText , yText);
+      
       }
     }
   }
-  
-  
 }
 
 
+
 function keyPressed(){
+
+  //sets x and y to which square the mouse is on
   let x = Math.floor(mouseX/cellSize);
   let y = Math.floor(mouseY/cellSize);
   
-  if(gridInitial[y][x] === 0 && state === "playing"){
+  //if the initial grid spot is empty and you are playing then depending on the key it changes the grid to that number
+  if(gridInitial[y][x] === OPEN_TILE && state === "playing"){
     if(key === "1"){
       grid[y][x] = 1;
-      
     }
     else if(key === "2"){
       grid[y][x] = 2;
@@ -307,15 +275,14 @@ function keyPressed(){
     else if(key === "9"){
       grid[y][x] = 9;
     }
-    // else{
-    //   state = "playing";
-    // }
   }
   
-  if(keyCode === BACKSPACE && gridInitial[y][x] === 0 ){
+  //if you press on the backspace and it is a square you can edit then you can delete 
+  if(keyCode === BACKSPACE && gridInitial[y][x] === OPEN_TILE ){
     grid[y][x] = 0;
   }
 
+  //if you press on s it changes the state to done
   if(keyCode === 83 && state === "playing"){
     state = "done";
   }
